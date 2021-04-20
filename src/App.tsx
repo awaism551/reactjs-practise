31a7344217064526dbe5a04694d248561ff336f1
Awais <awaism551@gmail.com>
2021-04-20 23:49:19 +0500

    functional components support

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
 interface TodoProps {}
-interface TodoState {
-  todos: Todo[];
-  inputValue: string;
-}
 interface Todo {
   id: number;
   desc: string;
 }
 
-class App extends React.Component<TodoProps, TodoState> {
-  constructor(props: TodoProps) {
-    super(props);
-    this.state = {
-      todos: [],
-      inputValue: "",
-    };
-  }
+const App = (props: TodoProps) => {
+  let [inputValue, setInputValue] = useState<string>("");
+  let [todos, setTodos] = useState<Todo[]>([]);
 
-  handleInputChange = (event: { target: HTMLInputElement }) => {
-    this.setState({
-      inputValue: event.target.value,
-    });
+  const handleInputChange = (event: { target: HTMLInputElement }) => {
+    debugger;
+    setInputValue(event.target.value);
   };
 
-  addTodo = (event: any) => {
-    var desc = this.state.inputValue;
-    if (desc) {
-      var id = this.state.todos[this.state.todos.length - 1]?.id + 1;
+  const addTodo = (event: any) => {
+    debugger;
+    if (inputValue) {
+      let id = todos[todos.length - 1]?.id + 1;
       if (!id) {
         id = 1;
       }
-      var todos = this.state.todos;
       todos.push({
         id,
-        desc,
+        desc: inputValue,
       });
-      this.setState({
-        todos,
-        inputValue: "",
-      });
+      setTodos(todos);
+      setInputValue("");
     }
     event.preventDefault();
   };
 
-  handleDelete = (id: number) => {
-    var todos = this.state.todos;
-    todos = todos.filter((todo) => todo.id !== id);
-    this.setState({
-      todos,
-    });
+  const handleDelete = (id: number) => {
+    debugger;
+    todos = todos.filter((todo) => todo?.id !== id);
+    setTodos(todos);
   };
 
-  render() {
-    const todos = this.state.todos.map((todo, index) => {
-      return (
-        <li key={todo.id}>
-          <div className="listItems">
-            <span>{todo.desc}</span>
-            <button onClick={() => this.handleDelete(todo.id)}>X</button>
-          </div>
-        </li>
-      );
-    });
+  const todosToRender = todos.map((todo) => {
     return (
-      <div className="App">
-        <h3>Todo List</h3>
-        <br />
-        <form onSubmit={this.addTodo}>
-          <div className="input">
-            <input
-              type="text"
-              value={this.state.inputValue}
-              onChange={this.handleInputChange}
-              autoFocus
-            />
-            <button type="submit">Add</button>
-          </div>
-        </form>
-        <div className="todos">
-          <ol className="todosList">{todos}</ol>
+      <li key={todo.id}>
+        <div className="listItems">
+          <span>{todo.desc}</span>
+          <button onClick={() => handleDelete(todo.id)}>X</button>
         </div>
-      </div>
+      </li>
     );
-  }
-}
+  });
+  return (
+    <div className="App">
+      <h3>Todo List</h3>
+      <br />
+      <form onSubmit={addTodo}>
+        <div className="input">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            autoFocus
+          />
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <div className="todos">
+        <ol className="todosList">{todosToRender}</ol>
+      </div>
+    </div>
+  );
+};
 
 export default App;
